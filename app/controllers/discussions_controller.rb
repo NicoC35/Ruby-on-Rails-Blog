212d5ceaec27e5ca6f9e659842ef1162213ca05b
ndqm88 <Nicholas.D.Quintero@gmail.com>
2019-12-10 14:53:36 -0500

    added destroy method to threads

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,7 +1,17 @@
 class DiscussionsController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret",
+  only: :destroy
+
   def create
     @comment = Comment.find(params[:comment_id])
     @discussion = @comment.discussions.create(discussion_params)
+    redirect_to comment_path(@comment)
+  end
+
+  def destroy
+    @comment = Comment.find(params[:comment_id])
+    @discussion = @comment.discussions.find(params[:id])
+    @discussion.destroy
     redirect_to comment_path(@comment)
   end
  
